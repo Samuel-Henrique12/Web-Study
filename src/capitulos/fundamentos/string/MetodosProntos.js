2e18990f168ee2097213163d2f540e623910d50e
Samuel-Henrique12 <sdikdplayd@gmail.com>
2025-08-26 10:18:52 -0300

    Testando métodos prontos da biblioteca padrão

---
+++ src/capitulos/fundamentos/string/MetodosProntos.js
@@ -0,0 +1,9 @@
+const teste = "teste";
+
+console.log(teste.charAt(4));
+console.log(teste.charCodeAt(3));
+console.log(teste.indexOf('e'));
+console.log(teste.substring(2,5));
+console.log(teste.replace('t','T'));
+console.log('Testando o '.concat(teste).concat("!"));
+console.log(teste.split('e'));
